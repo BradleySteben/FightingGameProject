75b2e5be5c2e25b0e46123ff3fe5d7b3d8faf880
Bradley Steben <snadley10@gmail.com>
2022-04-08 19:10:17 -0400

    Demo IMGs added

---
+++ Assets/Scripts/ControlCharacters.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCharacters : MonoBehaviour
+{
+    //Key to denying other moves or other stuff
+    private bool canAct = false;
+    private bool onGround = true;
+    private bool blocking = false;
+
+    [SerializeField]
+    float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D)){
+            transform.position = transform.position + new Vector3(speed,0,0) * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            transform.position = transform.position - new Vector3(speed,0,0) * Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate(){
+
+    }
+}
